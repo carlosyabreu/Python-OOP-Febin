2ecdd98241b9874603f0d6eb69aff58d78ca400e
carlosyabreu <carlos.abreu@zoho.com>
2022-06-13 19:06:03 +0100

    Add init.py

--- init.py
+++ init.py
@@ -1,11 +1,11 @@
 
 class Employee:
-    def __init__(self):
-        self.name = "Mark"
+    def __init__(self, name):
+        self.name = name
     def displayEmployeeDetails(self):
         print(self.name)
 
-employee = Employee()
+employee = Employee("Mark")
 employee.displayEmployeeDetails()
-employeeTwo = Employee()
+employeeTwo = Employee("Mattew")
 employeeTwo.displayEmployeeDetails()
